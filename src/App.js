f0be825e9aa32ae4e5af21c245c625add63832fb
Taíne Freitas <tddfreitas@inf.ufpel.edu.br>
2018-11-04 13:56:39 -0200

    With some bugs, but working!

--- src/App.js
+++ src/App.js
@@ -3,23 +3,113 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newTodo: '',
+      editing: false,
+      editingIndex: null,
+      todos: [{
+        id: 1, name: 'Play golf'
+      }, {
+        id: 2, name: 'Buy some clothes'
+      }, {
+        id: 3, name: 'Write some code'
+      }, {
+        id: 4, name: 'Watch Bahdcasts'
+      }]
+    };
+    this.updateTodo = this.updateTodo.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
+  addTodo() {
+    const newTodo = {
+      name: this.state.newTodo,
+      id: this.state.todos[this.state.todos.length - 1].id + 1
+    };
+
+    const todos = this.state.todos;
+    todos.push(newTodo);
+
+    this.setState({
+      todos: todos,
+      newTodo: ''
+    });
+  }
+  deleteTodo(index){
+    const todos = this.state.todos;
+    delete todos[index];
+
+    this.setState({todos});
+
+  }
+  editTodo(index){
+
+    const todo = this.state.todos[index];
+    this.setState({
+      editing: true,
+      newTodo: todo.name,
+      editingIndex: index
+    })
+  }
+  updateTodo(){
+    const todo = this.state.todos[this.state.editingIndex];
+
+    todo.name = this.state.newTodo;
+
+    const todos = this.state.todos;
+
+    todos[this.state.editingIndex] = todo;
+    this.setState({todos, editing:false, editingIndex:null, newTodo: ''});
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">CRUD React</h1>
         </header>
+        <div className="container">
+          <input type="text" 
+          name="todo" 
+          className="my-4 form-control"
+            placeholder="Add a new todo" 
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+          />
+        <button onClick={this.state.editing ? this.updateTodo : this.addTodo} 
+        className="btn-info mb-3 form-control">
+        {this.state.editing ? 'Editar Tarefa' : 'Adicionar Tarefa'}
+        </button>
+        {
+          !this.state.editing &&
+            <ul className="list-group">
+              {this.state.todos.map((item, index) => {
+                return <li key={item.id} className="list-group-item">
+                <button onClick = {() => {this.editTodo(index);}}
+                className="btn-sm mr-4 btn btn-success">
+                Editar
+                </button>
+                {item.name}
+                <button onClick = {() => {this.deleteTodo(index);}}
+                className="btn-sm ml-4 btn btn-danger">
+                Excluir
+                </button>
+                </li>
+              })}     
+            </ul>
+        }
+        </div>
       </div>
     );
   }
